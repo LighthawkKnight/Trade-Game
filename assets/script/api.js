bba984a16ce7c8b97ca591be381058df2f814299
administrator <administrator@adminisatorsMBP.localdomain>
2018-10-09 17:39:41 -0700

    add some comment on js file

--- assets/script/api.js
+++ assets/script/api.js
@@ -17,6 +17,7 @@ var boatSpeedFactor = 50;
 //days delay because of the weather
 var weatherCauseDelay = 0;
 var intervalID;
+
 // ship 1
 // var lineSymbol = {
 //   path:
@@ -36,17 +37,7 @@ var intervalID;
 //   rotation: 270,
 //   scale: 1.2
 // };
-// ship 2 reverse
-var lineSymbolR = {
-  path:
-    "M17,22 L26.9047619,22 L27,22 C27,24.2046438 25.2094214,26 23.0006308,26 L9.99936922,26 C7.79044819,26 6,24.209139 6,22 L6.0952381,22 L16,22 L16,7 L17,7 L17,22 L17,22 Z M27,21 L18,21 L18,12 L27,21 L27,21 Z M9,12.5 C6,17.5 6,21 6,21 L15,21 C15,21 14,19.5 14,15 C14,10.5 15,7 15,7 C15,7 12,7.5 9,12.5 L9,12.5 Z",
-  fillColor: "#0364f5",
-  fillOpacity: 1,
-  strokeColor: "#393",
-  rotation: 90,
-  scale: 1.2,
-  anchor: new google.maps.Point(600, 200)
-};
+
 //!--------------------------------------------------------------------------------
 //!------------MAIN FUNCTION--------------------------
 function initMap() {
@@ -73,7 +64,28 @@ function initMap() {
     scale: 0.1,
     anchor: new google.maps.Point(20, 400)
   };
+  // ship 2 reverse
+  // var lineSymbolR = {
+  //   path:
+  //     "M17,22 L26.9047619,22 L27,22 C27,24.2046438 25.2094214,26 23.0006308,26 L9.99936922,26 C7.79044819,26 6,24.209139 6,22 L6.0952381,22 L16,22 L16,7 L17,7 L17,22 L17,22 Z M27,21 L18,21 L18,12 L27,21 L27,21 Z M9,12.5 C6,17.5 6,21 6,21 L15,21 C15,21 14,19.5 14,15 C14,10.5 15,7 15,7 C15,7 12,7.5 9,12.5 L9,12.5 Z",
+  //   fillColor: "#0364f5",
+  //   fillOpacity: 1,
+  //   strokeColor: "#393",
+  //   rotation: 90,
+  //   scale: 1.2,
+  //   anchor: new google.maps.Point(600, 200)
+  // };
 
+  var lineSymbolR = {
+    path:
+      "M202.302,353.148c-46.343,23.113-90.541,45.085-146.523,61.376V305.988c0-8.448,4.182-16.346,11.166-21.096l41.807-28.428   c3.571-2.428,10.026-4.415,14.344-4.415h67.119v-39.534h75.523v39.534h57.871c9.393,0,17.006,7.614,17.006,17.006v23.501   C287.413,310.723,244.533,332.091,202.302,353.148z M190.215,173.476v20.514h75.523v-20.514c0-10.51-8.52-19.029-19.029-19.029   h-37.467C198.734,154.447,190.215,162.966,190.215,173.476z M146.352,113.665c5.434,0,10.604-1.118,15.307-3.122   c5.336,7.391,13.97,12.248,23.782,12.248c3.202,0,6.228-0.64,9.113-1.587c-0.046,0.532-0.16,1.044-0.16,1.587   c0,10.061,8.155,18.215,18.215,18.215s18.215-8.155,18.215-18.215c0-10.06-8.155-18.215-18.215-18.215   c-0.004,0-0.008,0.001-0.012,0.001c1.419-3.447,2.226-7.211,2.226-11.169c0-16.228-13.155-29.384-29.383-29.384   c-0.54,0-1.043,0.13-1.575,0.159c-4.575-16.508-19.549-28.695-37.513-28.695c-21.588,0-39.088,17.5-39.088,39.088   C107.264,96.164,124.764,113.665,146.352,113.665z M528.954,496.265c-3.778,5.754-3.881,13.155-0.061,18.881l22.525,33.761   c8.232,12.31-1.351,28.69-16.114,27.549L34.419,537.106c-13.626-1.07-25.016-10.651-28.489-23.872   C2.893,501.669,0,487.318,0,474.604c0-26.148,10.511-31.027,32.011-36.157c192.263-45.878,245.145-156.462,565.948-174.566   c8.333-0.47,15.035,6.81,13.919,15.081C602.372,349.385,568.794,435.601,528.954,496.265z M504.474,413.614   c0-4.7-3.804-8.504-8.504-8.504s-8.503,3.804-8.503,8.504c0,13.133-8.766,24.136-20.711,27.803v-58.622   c6.393-3.17,10.827-9.696,10.827-17.314c0-10.707-8.68-19.387-19.387-19.387c-10.706,0-19.386,8.68-19.386,19.387   c0,7.663,4.484,14.226,10.938,17.373v58.563c-11.945-3.667-20.711-14.67-20.711-27.803c0-4.7-3.804-8.504-8.504-8.504   s-8.504,3.804-8.504,8.504c0,25.485,20.735,46.221,46.221,46.221C483.738,459.835,504.474,439.1,504.474,413.614z",
+    fillColor: "#F00",
+    fillOpacity: 0.8,
+    strokeColor: "#393",
+    rotation: 90,
+    scale: 0.1,
+    anchor: new google.maps.Point(20, 400)
+  };
   // all the geography location used in this game.
   var houston = { lat: 29.795, lng: -95.236 };
   var miami = { lat: 25.813, lng: -80.134 };
@@ -93,14 +105,6 @@ function initMap() {
     center: lisbon,
     styles: [
       {
-        elementType: "geometry",
-        stylers: [
-          {
-            color: "#ebe3cd"
-          }
-        ]
-      },
-      {
         elementType: "labels",
         stylers: [
           {
@@ -109,22 +113,6 @@ function initMap() {
         ]
       },
       {
-        elementType: "labels.text.fill",
-        stylers: [
-          {
-            color: "#523735"
-          }
-        ]
-      },
-      {
-        elementType: "labels.text.stroke",
-        stylers: [
-          {
-            color: "#f5f1e6"
-          }
-        ]
-      },
-      {
         featureType: "administrative",
         elementType: "geometry",
         stylers: [
@@ -134,47 +122,33 @@ function initMap() {
         ]
       },
       {
-        featureType: "administrative",
-        elementType: "geometry.stroke",
+        featureType: "administrative.neighborhood",
         stylers: [
           {
-            color: "#c9b2a6"
+            visibility: "off"
           }
         ]
       },
       {
-        featureType: "administrative.country",
-        elementType: "labels.text",
+        featureType: "landscape.natural",
         stylers: [
+          {
+            color: "#bdac74"
+          },
           {
             visibility: "on"
           }
         ]
       },
       {
-        featureType: "administrative.country",
-        elementType: "labels.text.stroke",
-        stylers: [
-          {
-            weight: 8
-          }
-        ]
-      },
-      {
-        featureType: "landscape.natural.landcover",
+        featureType: "landscape.natural",
         elementType: "geometry.fill",
         stylers: [
           {
-            color: "#9d7550"
-          }
-        ]
-      },
-      {
-        featureType: "landscape.natural.terrain",
-        elementType: "geometry.fill",
-        stylers: [
+            color: "#bdac74"
+          },
           {
-            color: "#ae5811"
+            visibility: "on"
           }
         ]
       },
@@ -187,20 +161,10 @@ function initMap() {
         ]
       },
       {
-        featureType: "poi",
-        elementType: "geometry",
+        featureType: "road",
         stylers: [
           {
-            color: "#dfd2ae"
-          }
-        ]
-      },
-      {
-        featureType: "poi",
-        elementType: "labels.text.fill",
-        stylers: [
-          {
-            color: "#93817c"
+            visibility: "off"
           }
         ]
       },
@@ -214,17 +178,7 @@ function initMap() {
         ]
       },
       {
-        featureType: "water",
-        elementType: "geometry.fill",
-        stylers: [
-          {
-            color: "#b9d3c2"
-          }
-        ]
-      },
-      {
-        featureType: "water",
-        elementType: "geometry.stroke",
+        featureType: "transit",
         stylers: [
           {
             visibility: "off"
@@ -233,10 +187,24 @@ function initMap() {
       },
       {
         featureType: "water",
-        elementType: "labels.text.fill",
         stylers: [
           {
-            color: "#92998d"
+            color: "#26425d"
+          },
+          {
+            visibility: "on"
+          }
+        ]
+      },
+      {
+        featureType: "water",
+        elementType: "labels.text",
+        stylers: [
+          {
+            color: "#7d5e25"
+          },
+          {
+            visibility: "on"
           }
         ]
       }
@@ -271,14 +239,31 @@ function initMap() {
   preDefinePath.setMap(map);
 
   // draw the marker on the map
-  var huston_marker = new google.maps.Marker({ position: houston, map: map });
-  var lisbon_marker = new google.maps.Marker({ position: lisbon, map: map });
+  var huston_marker = new google.maps.Marker({
+    position: houston,
+    map: map,
+    icon: "assets/image/port_marker/port-map-marker-point.svg"
+  });
+  var lisbon_marker = new google.maps.Marker({
+    position: lisbon,
+    map: map,
+    icon: "assets/image/port_marker/port-map-marker-point.svg"
+  });
   var elizabeth_sa_marker = new google.maps.Marker({
     position: elizabeth_sa,
-    map: map
+    map: map,
+    icon: "assets/image/port_marker/port-map-marker-point.svg"
   });
-  var mumbai_marker = new google.maps.Marker({ position: mumbai, map: map });
-  var miami_marker = new google.maps.Marker({ position: miami, map: map });
+  var mumbai_marker = new google.maps.Marker({
+    position: mumbai,
+    map: map,
+    icon: "assets/image/port_marker/port-map-marker-point.svg"
+  });
+  var miami_marker = new google.maps.Marker({
+    position: miami,
+    map: map,
+    icon: "assets/image/port_marker/port-map-marker-point.svg"
+  });
   var lh1_marker = new google.maps.Marker({
     position: lightHouse1,
     map: map,
@@ -350,6 +335,7 @@ function initMap() {
   });
 
   $("#routeConfirm").on("click", function initMap() {
+    clearInterval(intervalID);
     // starting point index in preDefinePath array
     var a = marinePlanCoordinates[0];
     //ending point index in perDefinePath array
@@ -392,13 +378,14 @@ function initMap() {
         icons: [
           {
             icon: lineSymbolR,
-            offset: "100%"
+            offset: "90px"
           }
         ]
       });
       marinePath_R_L.setMap(map);
       animateBoat(marinePath_R_L);
     }
+
     function animateBoat(line) {
       var count = 0;
       var intervalID = window.setInterval(function() {
